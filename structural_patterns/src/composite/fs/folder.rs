f55053a36f260158dec4919b8edb4a2a811f2083
mmeszaros <Marcell Mszros>
2026-02-22 16:52:06 +0100

    Composite pattern with life time

--- structural_patterns/src/composite/fs/folder.rs
+++ structural_patterns/src/composite/fs/folder.rs
@@ -1,22 +1,22 @@
 use super::Component;
 
-pub struct Folder {
+pub struct Folder<'a> {
     name: &'static str,
-    components: Vec<Box<dyn Component>>,
+    components: Vec<Box<dyn Component + 'a >>,
 }
 
-impl Folder {
+impl<'a> Folder<'a> {
     pub fn new(name: &'static str) -> Self {
         Self { name, components: vec![] }
     }
 
-    pub fn add(&mut self, component: impl Component + 'static) {
+    pub fn add(&mut self, component: impl Component + 'a) {
         self.components.push(Box::new(component));
     }
 
 }
 
-impl Component for Folder  {
+impl Component for Folder<'_>  {
     fn search(&self, keyword: &str) {
         println!(
             "Searching recursively for keyword {} in folder {}",
